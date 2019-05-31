c91a162272905eeea55b8919ae4f80994fc546d2
kathylai <46954111+kathylai@users.noreply.github.com>
2019-05-31 11:42:33 +0800

    Add files via upload

--- pca.R
+++ pca.R
@@ -1,9 +1,9 @@
-d = read.csv("train.csv",header = TRUE)
+d = read.csv("train1.csv",header = TRUE)
 dc = d[,2:9]
 sdc = scale(dc)#标准化
 mean(sdc[,1])
 cov_sdc = cov(sdc)
-eigen(cov_sdc)
+eigen(cov_sdc)#算出的八个特征向量各自的维度 求特征值 八个特征值 累计结果大于85%
 princomp(dc)
 library("stats",lib.loc = "C:/Program Files/Microsoft/R Open/R-3.4.0/library")
 prcomp(dc)
